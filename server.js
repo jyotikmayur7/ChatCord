76d1ef8c19fd8195a8bd22401d3a94c7f26b1247
orangekun9 <heyjyotik@gmail.com>
2020-08-08 11:59:15 +0100

    Adding socket.io in both frontend as well as in backend

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.listen(3000, () => {
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket => {
+    console.log('New Web Socket Connection');
+})
+
+server.listen(3000, () => {
     console.log(`Server running on port 3000`);
 })
