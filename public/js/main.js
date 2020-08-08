9c76bb854a95057012cb9b0276b56091e58cce7e
orangekun9 <heyjyotik@gmail.com>
2020-08-08 13:11:34 +0100

    Added feature to append new html div in message container whenever new message arrives

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,10 @@ const chatForm = document.getElementById('chat-form');
 
 const socket = io();
 
+// Message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
 
 // Message submit
@@ -16,4 +18,16 @@ chatForm.addEventListener('submit', (e) => {
 
     // Emit message to the server
     socket.emit('chatMessage', msg);
-}); 
+});
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+    <p class="meta">Mary <span>9:15pm</span></p>
+    <p class="text">${message}</p>
+    `;
+
+    document.querySelector(".chat-messages").appendChild(div);
+}
