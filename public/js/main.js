68fb9722a10e0275d7a86dd27259bffd0f9e2835
orangekun9 <heyjyotik@gmail.com>
2020-08-09 11:21:51 +0100

    Adding message object to the server end and using it in the client end

--- public/js/main.js
+++ public/js/main.js
@@ -34,8 +34,8 @@ function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
     div.innerHTML = `
-    <p class="meta">Mary <span>9:15pm</span></p>
-    <p class="text">${message}</p>
+    <p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">${message.text}</p>
     `;
 
     document.querySelector(".chat-messages").appendChild(div);
--- server.js
+++ server.js
@@ -10,25 +10,26 @@ const formatMessage = require('./utils/messages.js');
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+const botName = 'ChatCord Bot';
 
 // Run when a client connects
 io.on('connection', socket => {
 
     // Welcome current user
-    socket.emit('message', 'Welcome to ChatCord');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord'));
 
     // Broadcast when a user connects (sends message to everyone except the user who just got connected)
-    socket.broadcast.emit('message', 'A user has joined that chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined that chat'));
 
     // Runs when user disconnects (sends message to everyone except the user who just got disconnected)
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left that chat');
+        io.emit('message', formatMessage(botName, 'A user has left that chat'));
     });
 
     // Listen for chat message
     socket.on('chatMessage', (msg) => {
         // Sending the incoming user's chat message to everyone
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     })
 })
 
